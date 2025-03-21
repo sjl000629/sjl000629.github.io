c288b92491a3528d90eeeaee431da2d9c6a4679a
ShangJunLong <1472912024@qq.com>
2025-03-21 16:22:40 +0800

    init

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   outDir: 'docs',
-  title: "My Awesome Project",
+  title: "Mr.Shang Awesome Project",
   description: "A VitePress Site",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
@@ -23,7 +23,6 @@ export default defineConfig({
         ]
       }
     ],
-
 
   }
 })
