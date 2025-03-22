ca8b21abd2dce61a7445b99922b30a752801c251
ShangJunLong <1472912024@qq.com>
2025-03-22 14:16:44 +0800

    中文

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,29 +1,30 @@
-import { defineConfig } from 'vitepress'
+import {defineConfig} from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/',
-  title: "Mr.Shang Awesome Project",
-  description: "A VitePress Site",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ],
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' },
-          { text: 'Test Vue', link: '/test-vue' },
-        ]
-      }
-    ],
+    base: '/',
+    lang: 'zh-CN',
+    title: "Mr.Shang Awesome Project",
+    description: "A VitePress Site",
+    themeConfig: {
+        // https://vitepress.dev/reference/default-theme-config
+        nav: [
+            {text: 'Home', link: '/'},
+            {text: 'Examples', link: '/markdown-examples'}
+        ],
+        socialLinks: [
+            {icon: 'github', link: 'https://github.com/vuejs/vitepress'}
+        ],
+        sidebar: [
+            {
+                text: 'Examples',
+                items: [
+                    {text: 'Markdown Examples', link: '/markdown-examples'},
+                    {text: 'Runtime API Examples', link: '/api-examples'},
+                    {text: 'Test Vue', link: '/test-vue'},
+                ]
+            }
+        ],
 
-  }
+    }
 })
